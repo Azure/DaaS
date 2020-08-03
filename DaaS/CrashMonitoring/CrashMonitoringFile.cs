26ad677f8e0a9b93e36bfd033aa766e4099256c3
Puneet Gupta <puneetg@microsoft.com>
2020-08-03 20:25:08 +0530

    Correcting file names in file header

--- DaaS/CrashMonitoring/CrashMonitoringFile.cs
+++ DaaS/CrashMonitoring/CrashMonitoringFile.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="LoggingHandler.cs" company="Microsoft Corporation">
+// <copyright file="CrashMonitoringFile.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 // </copyright>
