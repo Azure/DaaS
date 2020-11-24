1755db5ed1e2ba6e7b11a0449fe81fb58dcac228
Vijay Saayi R <visaayir@microsoft.com>
2020-11-24 21:03:09 +0530

    Removed unnecessary namespace imports

--- DiagnosticsExtension/Controllers/MsiValidatorController.cs
+++ DiagnosticsExtension/Controllers/MsiValidatorController.cs
@@ -3,11 +3,9 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 
 namespace DiagnosticsExtension.Controllers
