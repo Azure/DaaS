a5525784231ef30d007fe33e1ca52e9165630f9d
Yifu Guo <yifguo@microsoft.com>
2021-05-13 14:20:53 -0700

    update

--- DiagnosticsExtension/Controllers/UdpEchoTestController.cs
+++ DiagnosticsExtension/Controllers/UdpEchoTestController.cs
@@ -1,25 +1,12 @@
 //-----------------------------------------------------------------------
-// <copyright file="DatabaseTestController.cs" company="Microsoft Corporation">
+// <copyright file="UdpEchoTestController.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 // </copyright>
 //-----------------------------------------------------------------------
 
-using DiagnosticsExtension.Models;
-using Microsoft.WindowsAzure.Storage;
-using MySql.Data.MySqlClient;
-using Newtonsoft.Json;
-using Npgsql;
-using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Common;
-using System.Data.Entity.Core.EntityClient;
-using System.Data.Odbc;
-using System.Data.SqlClient;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
