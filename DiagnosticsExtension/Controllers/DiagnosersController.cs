8a7d80f8923c2d1b42b135cb8b9f187e8f1a3af6
Puneet Gupta <puneetg@microsoft.com>
2021-05-26 18:32:43 +0530

    Adding check for RPS ASE

--- DiagnosticsExtension/Controllers/DiagnosersController.cs
+++ DiagnosticsExtension/Controllers/DiagnosersController.cs
@@ -5,36 +5,43 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using DiagnosticsExtension.Models;
-using DaaS.Diagnostics;
-using DaaS.Sessions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DiagnosticsExtension.Models;
+using DaaS.Diagnostics;
+using DaaS.Sessions;
 
 namespace DiagnosticsExtension.Controllers
 {
     public class DiagnosersController : ApiController
     {
+        private const string RelayPartySuiteErrorMessage = "Azure App Service sandbox component is disabled. This can happen if the App is running on ILB ASE with Relay Party suite service enabled. For this configuration, none of the diagnostic tools work.";
+
         public HttpResponseMessage Get()
         {
             try
             {
-                List<DiagnoserDetails> retVal = new List<DiagnoserDetails>();
                 SessionController sessionController = new SessionController();
+                if (!sessionController.IsSandboxAvailable())
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, RelayPartySuiteErrorMessage);
+                }
+
+                List<DiagnoserDetails> retVal = new List<DiagnoserDetails>();
                 foreach (Diagnoser diagnoser in sessionController.GetAllDiagnosers())
                 {
                     retVal.Add(new DiagnoserDetails(diagnoser));
                 }
+
                 return Request.CreateResponse(HttpStatusCode.OK, retVal);
             }
             catch (Exception ex)
             {
                 DaaS.Logger.LogErrorEvent("Encountered exception while getting diagnosers", ex);
-                return Request.CreateErrorResponse(HttpStatusCode.OK, ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
             }
         }
     }
