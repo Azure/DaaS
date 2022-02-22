fa9e317161aca212d56fe29ce5a93aae13272e06
Puneet Gupta <puneetg@microsoft.com>
2022-02-22 17:28:05 +0530

    Adding Unit tests project

---
+++ Daas.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Daas.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft IT")]
+[assembly: AssemblyProduct("Daas.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft IT 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29699cc9-8a41-4b2f-b152-1d6f0fb7842f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Daas.Tests/TestHelpers.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TestHelpers.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DaaS;
+
+namespace Daas.Tests
+{
+    internal static class TestHelpers
+    {
+        const string temporaryDirectory = "c:\\temp\\daas-testing";
+        public static void SetupTestEnvironment()
+        {
+            Environment.SetEnvironmentVariable("HOME", temporaryDirectory);
+            Environment.SetEnvironmentVariable("WEBSITE_COMPUTE_MODE", "Dedicated");
+            CleanupTemporaryDirectory();
+        }
+
+        private static void CleanupTemporaryDirectory()
+        {
+            if (Directory.Exists(temporaryDirectory))
+            {
+                FileSystemHelpers.DeleteDirectoryContentsSafe(temporaryDirectory);
+                FileSystemHelpers.DeleteDirectorySafe(temporaryDirectory);
+            }
+
+            Directory.CreateDirectory(temporaryDirectory);
+        }
+    }
+}
