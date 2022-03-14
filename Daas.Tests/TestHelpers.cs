132976111680e1b394ae9ec05235c3d10ef06e92
Puneet Gupta <puneetg@microsoft.com>
2022-03-14 22:54:33 +0530

    Correctings tests project

--- Daas.Tests/TestHelpers.cs
+++ Daas.Tests/TestHelpers.cs
@@ -1,39 +1,28 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="TestHelpers.cs" company="Microsoft Corporation">
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-// </copyright>
-// -----------------------------------------------------------------------
-
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DaaS;
 
-namespace Daas.Tests
+namespace Daas.Test
 {
     internal static class TestHelpers
     {
-        const string temporaryDirectory = "c:\\temp\\daas-testing";
+        const string TemporaryDirectory = "c:\\temp\\daas-testing";
         public static void SetupTestEnvironment()
         {
-            Environment.SetEnvironmentVariable("HOME", temporaryDirectory);
+            Environment.SetEnvironmentVariable("HOME", TemporaryDirectory);
             Environment.SetEnvironmentVariable("WEBSITE_COMPUTE_MODE", "Dedicated");
             CleanupTemporaryDirectory();
         }
 
         private static void CleanupTemporaryDirectory()
         {
-            if (Directory.Exists(temporaryDirectory))
+            if (Directory.Exists(TemporaryDirectory))
             {
-                FileSystemHelpers.DeleteDirectoryContentsSafe(temporaryDirectory);
-                FileSystemHelpers.DeleteDirectorySafe(temporaryDirectory);
+                FileSystemHelpers.DeleteDirectoryContentsSafe(TemporaryDirectory);
+                FileSystemHelpers.DeleteDirectorySafe(TemporaryDirectory);
             }
 
-            Directory.CreateDirectory(temporaryDirectory);
+            Directory.CreateDirectory(TemporaryDirectory);
         }
     }
 }
