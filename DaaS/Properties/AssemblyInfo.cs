34dfffd7139bd52f45a93ca8c5849148520d4c58
Puneet Gupta <puneetg@microsoft.com>
2022-06-06 19:42:13 +0530

    Setting version to 2.13

--- DaaS/Properties/AssemblyInfo.cs
+++ DaaS/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("2.12.22606.01")]
-[assembly: AssemblyFileVersion("2.12.22606.01")]
+[assembly: AssemblyVersion("2.13.22606.01")]
+[assembly: AssemblyFileVersion("2.13.22606.01")]
