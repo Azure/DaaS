3e8d79884641cd578b42e6e9e33560330cab2032
Puneet Gupta <puneetg@microsoft.com>
2022-08-25 21:47:37 +0530

    DAAS Version 3

--- Daas.Tests/E2eTests.cs
+++ Daas.Tests/E2eTests.cs
@@ -101,7 +101,7 @@ namespace Daas.Test
             foreach (var file in knownFiles)
             {
                 _output.WriteLine($"Checking for file {file}");
-                //var resp = await _client.GetAsync($"api/vfs/SystemDrive/Program%20Files%20(x86)/SiteExtensions/DaaS/2.20.22824.01/{file}");
+                //var resp = await _client.GetAsync($"api/vfs/SystemDrive/Program%20Files%20(x86)/SiteExtensions/DaaS/3.0.22824.01/{file}");
                 var resp = await _client.GetAsync($"api/vfs/siteextensions/daas/{file}");
                 Assert.True(resp.IsSuccessStatusCode);
             }
