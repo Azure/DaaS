4a13d3155f48bfaa2ac4c11e8ce55bf3c862e051
Puneet Gupta <puneetg@microsoft.com>
2022-10-19 15:18:19 +0530

    DaaS version update to Oct 2022

--- DaaSRunner/Properties/AssemblyInfo.cs
+++ DaaSRunner/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("3.2.22826.01")]
-[assembly: AssemblyFileVersion("3.2.22826.01")]
+[assembly: AssemblyVersion("3.2.22109.01")]
+[assembly: AssemblyFileVersion("3.2.22109.01")]
