aae7e6860aea12c08eaeb943c2afed9b1cac0881
Puneet Gupta <puneetg@microsoft.com>
2023-02-01 12:03:19 +0530

    Change from HTTP GET to HTTP POST for sensitive controllers (#90)
    * Changing to HTTP POST for sensitive controllers
    
    * Updating DaaS version

---
+++ DiagnosticsExtension/Models/ConnectionStringValidator/AppSettingRequestBody.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AppSettingRequestBody.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DiagnosticsExtension.Models.ConnectionStringValidator
+{
+    public class AppSettingRequestBody
+    {
+        public string AppSettingName { get; set; }
+        public string Type { get; set; }
+    }
+}
