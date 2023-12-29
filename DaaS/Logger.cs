de0cccd9b395030901113d2650c200f6d8fbbdf2
Puneet Gupta <puneetg@microsoft.com>
2023-12-29 21:58:52 +0530

    Minor console writelinefixes (#125)
    * Console and logging changes for DiagLauncher
    
    * Fixing stopwatch code

--- DaaS/Logger.cs
+++ DaaS/Logger.cs
@@ -335,6 +335,7 @@ namespace DaaS
 
         public static void LogStatus(string message)
         {
+            Console.WriteLine(message);
             LogDiagnoserEvent(message);
             if (!string.IsNullOrWhiteSpace(StatusFile))
             {
